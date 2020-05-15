2f0407f98b5b151255481eea5b64c37018540f24
kris <2987583772@qq.com>
2020-05-15 17:32:56 +0800

    Initial commit

---
+++ src/test/TestSource.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * @author Administrator
+ * @create 2020-05-15 17:17
+ */
+public class TestSource {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+    }
+}
